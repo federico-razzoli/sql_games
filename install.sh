88d3592691bcd1fffa4bc11e8b8e7d13b94c6575
Federico Razzoli <federico.razzoli@vettabase.com>
2023-07-09 14:53:04 +0100

    Replace global install.sql with install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+
+cd anagram     ;  mariadb < ./install.sql;  cd ..
+cd bulls_cows  ;  mariadb < ./install.sql;  cd ..
+cd hangman     ;  mariadb < ./install.sql;  cd ..
--- install.sql
+++
@@ -1,5 +0,0 @@
-# This file installs all games from the sql_games project.
-
-\. ./anagram/install.sql
-\. ./bulls_cows/install.sql
-\. ./hangman/install.sql
