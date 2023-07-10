613642ba97e3ed7915a0b16093f96ece61bfae60
Federico Razzoli <federico.razzoli@vettabase.com>
2023-07-10 03:47:53 +0100

    Add explanatory comment in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 
+# Each game can be installed individually by running the `install.sql` file in its
+# directory. This file simply calls all `install.sql` files one by one.
+# Sorry if you expected something fancy.
+
+
 cd anagram     ;  mariadb < ./install.sql;  cd ..
 cd bulls_cows  ;  mariadb < ./install.sql;  cd ..
 cd hangman     ;  mariadb < ./install.sql;  cd ..
